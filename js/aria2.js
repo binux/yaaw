31ed352fb2e645ee7008a6900f2c1c9648eae5a7
Binux <17175297.hk@gmail.com>
2012-05-04 00:15:08 +0800

    update

--- js/aria2.js
+++ js/aria2.js
@@ -1,5 +1,5 @@
 if (typeof ARIA2=="undefined"||!ARIA2) var ARIA2=(function(){
-    var jsonrpc_interface;
+    var jsonrpc_interface, interval_id;
 
     function get_error(result) {
         if (typeof result == "string")
@@ -14,8 +14,26 @@ if (typeof ARIA2=="undefined"||!ARIA2) var ARIA2=(function(){
         waiting: "icon-time",
         paused: "icon-pause",
         error: "icon-warning-sign",
-        complete: "icon-ok",
+        complete: "icon-inbox",
         removed: "icon-remove",
+    };
+
+    function default_error(result) {
+        console.debug(result);
+
+        var error_msg = get_error(result);
+
+        $("#main-alert .alert").attr("class", "alert alert-error");
+        $("#main-alert .alert-msg").html("<strong>Error: </strong>"+error_msg);
+        $("#main-alert").show();
+    }
+
+    function main_alert(_class, msg, timeout) {
+        $("#main-alert .alert").attr("class", "alert "+_class);
+        $("#main-alert .alert-msg").html(msg);
+        $("#main-alert").show();
+        if (timeout)
+            window.setTimeout(function() { $("#main-alert").fadeOut(); }, timeout);
     }
 
     return {
@@ -26,6 +44,8 @@ if (typeof ARIA2=="undefined"||!ARIA2) var ARIA2=(function(){
         },
 
         request: function(method, params, success, error) {
+            if (error == undefined)
+                error = default_error;
             $.jsonRPC.request(method, {params:params, success:success, error:error});
         },
 
@@ -39,6 +59,7 @@ if (typeof ARIA2=="undefined"||!ARIA2) var ARIA2=(function(){
                     //id:1
                     //jsonrpc:"2.0"
                     //result:"2"
+                    ARIA2.refresh();
                     $("#add-task-modal").modal('hide');
                     $("#add-task-modal uri-input").val("");
                     $("#add-task-alert").hide();
@@ -60,48 +81,54 @@ if (typeof ARIA2=="undefined"||!ARIA2) var ARIA2=(function(){
                     console.debug(result);
 
                     if (!result.result) {
-                        $("#main-alert .alert").addClass("alert-error");
-                        $("#main-alert .alert-msg").html("<strong>Error: </strong>rpc result error.");
-                        $("#main-alert").show();
+                        main_alert("alert-error", "<strong>Error: </strong>rpc result error.", 5000);
                     }
 
                     result = ARIA2.status_fix(result.result);
-                    $("#active-tasks-table").append($("#task-tpl").mustache({"tasks": result}));
-                },
+                    $("#active-tasks-table").empty().append($("#active-task-tpl").mustache({"tasks": result})).find("[rel=tooltip]").tooltip({"placement": "bottom"});
+                }
+            );
+        },
+
+        tell_waiting: function(keys) {
+            var params = [0, 1000];
+            if (keys) params.push(keys);
+            ARIA2.request("tellWaiting", params,
                 function(result) {
                     console.debug(result);
 
-                    var error_msg = get_error(result);
+                    if (!result.result) {
+                        main_alert("alert-error", "<strong>Error: </strong>rpc result error.", 5000);
+                    }
 
-                    $("#main-alert .alert").addClass("alert-error");
-                    $("#main-alert .alert-msg").html("<strong>Error: </strong>"+error_msg);
-                    $("#main-alert").show();
+                    result = ARIA2.status_fix(result.result);
+                    $("#waiting-tasks-table").empty().append($("#other-task-tpl").mustache({"tasks": result})).find("[rel=tooltip]").tooltip({"placement": "bottom"});
+
+                    if ($("#other-tasks .task").length == 0)
+                        $("#waiting-tasks-table").append($("#other-task-empty").text())
                 }
             );
         },
 
         tell_stoped: function(keys) {
-            ARIA2.request("tellStopped", [0, 100],
+            var params = [0, 1000];
+            if (keys) params.push(keys);
+            ARIA2.request("tellStopped", params,
                 function(result) {
                     console.debug(result);
 
                     if (!result.result) {
-                        $("#main-alert .alert").addClass("alert-error");
-                        $("#main-alert .alert-msg").html("<strong>Error: </strong>rpc result error.");
-                        $("#main-alert").show();
+                        main_alert("alert-error", "<strong>Error: </strong>rpc result error.", 5000);
                     }
 
                     result = ARIA2.status_fix(result.result);
-                    $("#active-tasks-table").append($("#task-tpl").mustache({"tasks": result}));
-                },
-                function(result) {
-                    console.debug(result);
+                    $("#stoped-tasks-table").empty().append($("#other-task-tpl").mustache({"tasks": result})).find("[rel=tooltip]").tooltip({"placement": "bottom"});
 
-                    var error_msg = get_error(result);
+                    if ($("#waiting-tasks-table .empty-tasks").length > 0 &&
+                        $("#stoped-tasks-table .task").length > 0) {
+                            $("#waiting-tasks-table").empty();
+                        }
 
-                    $("#main-alert .alert").addClass("alert-error");
-                    $("#main-alert .alert-msg").html("<strong>Error: </strong>"+error_msg);
-                    $("#main-alert").show();
                 }
             );
         },
@@ -116,6 +143,8 @@ if (typeof ARIA2=="undefined"||!ARIA2) var ARIA2=(function(){
 
                     title = title.replace(new RegExp("^"+dir+"/?"), "").split("/");
                     title = title[0]
+                    if (title.length == 0)
+                        title = "Unknown";
 
                     if (result.files.length > 1)
                         title += " ("+result.files.length+ " files..)"
@@ -136,22 +165,97 @@ if (typeof ARIA2=="undefined"||!ARIA2) var ARIA2=(function(){
                     return size.toFixed(2)+" "+format_text[i];
                 }
             }
+            var time_interval = [60, 60, 24];
+            var time_text = ["s", "m", "h"];
+            function format_time(time) {
+                if (time == Infinity) {
+                    return "INF";
+                } else if (time == 0) {
+                    return "0s";
+                }
+
+                time = Math.floor(time);
+                var i = 0;
+                var result = "";
+                while (time > 0 && i < 3) {
+                    result = time % time_interval[i] + time_text[i] + result;
+                    time = Math.floor(time/time_interval[i]);
+                    i++;
+                }
+                if (time > 0) {
+                    result = time + "d" + result;
+                }
+                return result;
+            }
             for (var i=0; i<results.length; i++) {
                 var result = results[i];
 
                 result.status_icon = status_icon_map[result.status];
                 result.title = get_title(result);
-                result.progress = result.completedLength * 1.0 / result.totalLength * 100;
+                if (result.totalLength == 0)
+                    result.progress = 0;
+                else
+                    result.progress = (result.completedLength * 1.0 / result.totalLength * 100).toFixed(2);
+                result.etc = format_time((result.totalLength - result.completedLength)/result.downloadSpeed)
+
                 result.completedLength = format_size(result.completedLength);
-                result.totalLength = format_size(result.totalLength);
                 result.uploadLength = format_size(result.uploadLength);
+                result.totalLength = format_size(result.totalLength);
                 result.uploadSpeed = format_size(result.uploadSpeed);
                 result.downloadSpeed = format_size(result.downloadSpeed);
-                result.uploadLength = format_size(result.uploadLength);
+
 
                 result.numSeeders = parseInt(result.numSeeders);
+                result.connections = parseInt(result.connections);
             }
             return results;
+        },
+
+        pause_all: function() {
+            ARIA2.request("pauseAll", [],
+                function(result) {
+                    console.debug(result);
+
+                    ARIA2.refresh();
+                    main_alert("alert-info", "Paused all tasks. Please wait for action such as contacting BitTorrent tracker.", 2000);
+                }
+            );
+        },
+
+        unpause_all: function() {
+            ARIA2.request("unpauseAll", [],
+                function(result) {
+                    console.debug(result);
+
+                    ARIA2.refresh();
+                    main_alert("alert-info", "Unpaused all tasks.", 2000);
+                }
+            );
+        },
+
+        purge_download_result: function() {
+            ARIA2.request("purgeDownloadResult", [],
+                function(result) {
+                    console.debug(result);
+
+                    ARIA2.refresh();
+                    main_alert("alert-info", "Removed all completed/error/removed downloads tasks.", 2000);
+                }
+            );
+        },
+
+        refresh: function() {
+            ARIA2.tell_active();
+            ARIA2.tell_waiting();
+            ARIA2.tell_stoped();
+        },
+
+        auto_refresh: function(interval) {
+            if (interval == undefined)
+                interval = 5000;
+            if (interval_id)
+                window.clearInterval(interval_id);
+            interval_id = window.setInterval(function() { ARIA2.tell_active(); }, interval);
         }
 
     }
