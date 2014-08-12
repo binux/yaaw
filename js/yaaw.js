92c36e04bb7a21ba7589721353b2b6312a72f65a
aa65535 <aa65535@live.com>
2014-08-12 18:22:53 +0800

    Using explicit protocol for JSON-RPC Path
    fix 186f423

--- js/yaaw.js
+++ js/yaaw.js
@@ -690,7 +690,7 @@ var YAAW = (function() {
 
     setting: {
       init: function() {
-        this.jsonrpc_path = $.Storage.get("jsonrpc_path") || "//"+(location.host.split(":")[0]||"localhost")+":6800"+"/jsonrpc";
+        this.jsonrpc_path = $.Storage.get("jsonrpc_path") || location.protocol+"//"+(location.host.split(":")[0]||"localhost")+":6800"+"/jsonrpc";
         this.refresh_interval = Number($.Storage.get("refresh_interval") || 10000);
         this.add_task_option = $.Storage.get("add_task_option");
         this.jsonrpc_history = JSON.parse($.Storage.get("jsonrpc_history") || "[]");
